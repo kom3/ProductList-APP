ccacda517d6e82609541d7e4112dbbd5a9c98143
Anoite Team <askaeks@gmail.com>
2019-01-28 21:56:58 +0700

    Edit some style

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,34 +1,35 @@
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
-  final String title, imageUrl;
+  final String title, imageUrl, description;
+  final double price;
 
-  ProductPage(this.title, this.imageUrl);
+  ProductPage(this.title, this.imageUrl, this.price, this.description);
 
-  _showWarningDIalog(BuildContext context) {
-    return showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Are you sure?'),
-            content: Text('This action cannot be undine!'),
-            actions: <Widget>[
-              FlatButton(
-                  child: Text('Continue'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                    Navigator.pop(context, true);
-                  }),
-              FlatButton(
-                child: Text('Discard'),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              )
-            ],
-          );
-        });
-  }
+  // _showWarningDIalog(BuildContext context) {
+  //   return showDialog(
+  //       context: context,
+  //       builder: (BuildContext context) {
+  //         return AlertDialog(
+  //           title: Text('Are you sure?'),
+  //           content: Text('This action cannot be undine!'),
+  //           actions: <Widget>[
+  //             FlatButton(
+  //                 child: Text('Continue'),
+  //                 onPressed: () {
+  //                   Navigator.pop(context);
+  //                   Navigator.pop(context, true);
+  //                 }),
+  //             FlatButton(
+  //               child: Text('Discard'),
+  //               onPressed: () {
+  //                 Navigator.pop(context);
+  //               },
+  //             )
+  //           ],
+  //         );
+  //       });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -47,13 +48,38 @@ class ProductPage extends StatelessWidget {
             Image.asset(imageUrl),
             Container(
               padding: EdgeInsets.all(10.0),
-              child: Text(title),
+              child: Text(
+                title,
+                style: TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
+              ),
             ),
-            RaisedButton(
-              color: Theme.of(context).primaryColor,
-              textColor: Colors.white,
-              child: Text('Delete'),
-              onPressed: () => _showWarningDIalog(context),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Surakarta, Central Java, Indonesia',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Text(
+                    '|',
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ),
+                Text(
+                  '\$' + price.toString(),
+                  style: TextStyle(color: Colors.grey),
+                )
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              alignment: Alignment.center,
+              child: Text(
+                description,
+                textAlign: TextAlign.center,
+              ),
             )
           ],
         ),
