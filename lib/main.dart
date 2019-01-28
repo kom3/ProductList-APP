0c3f912997e0bd5e40c549439b2bca099bd53b27
Anoite Team <askaeks@gmail.com>
2019-01-28 09:03:28 +0700

    Creating auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-
+import 'pages/auth.dart';
 import 'pages/products_admin.dart';
 import 'pages/products.dart';
 import 'pages/product.dart';
@@ -41,8 +41,8 @@ class _MyAppState extends State<MyApp> {
           accentColor: Colors.blueAccent),
       // home: AuthPage(),
       routes: {
-        '/': (BuildContext context) =>
-            ProductsPage(_products),
+        '/': (BuildContext context) => AuthPage(),
+        '/products': (BuildContext context) => ProductsPage(_products),
         '/admin': (BuildContext context) => ProductsAdminPage(_addProduct, _deleteProduct),
       },
       onGenerateRoute: (RouteSettings settings) {
--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -2,21 +2,68 @@ import 'package:flutter/material.dart';
 
 // import 'products.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AuthPageState();
+  }
+}
+
+class _AuthPageState extends State<AuthPage> {
+  String _emailValue;
+  String _passwordValue;
+  bool _acceptTermsValue = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text(
-            'Login',
-          ),
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/');
-          },
+      body: Container(
+        margin: EdgeInsets.all(10.0),
+        child: ListView(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'E-Mail'),
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (String value) {
+                setState(() {
+                  _emailValue = value;
+                });
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+              onChanged: (String value) {
+                setState(() {
+                  _passwordValue = value;
+                });
+              },
+            ),
+            SwitchListTile(
+              onChanged: (bool value) {
+                setState(() {
+                  _acceptTermsValue = value;
+                });
+              },
+              value: _acceptTermsValue,
+              title: Text('Accept Terms'),
+            ),
+            RaisedButton(
+              color: Theme.of(context).primaryColor,
+              textColor: Colors.white,
+              child: Text(
+                'Login',
+              ),
+              onPressed: () {
+                print(_emailValue);
+                print(_passwordValue);
+                Navigator.pushReplacementNamed(context, '/products');
+              },
+            ),
+          ],
         ),
       ),
     );
