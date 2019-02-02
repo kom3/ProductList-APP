91c101eaf3f9b7f8abd17ea8b0e78ed724b3402d
Anoite Team <askaeks@gmail.com>
2019-02-02 22:32:52 +0700

    Submitting data with form data

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -12,9 +12,16 @@ class ProductCreatePage extends StatefulWidget {
 }
 
 class _ProductCreatePage extends State<ProductCreatePage> {
-  String _titleValue = '';
-  String _descriptionValue = '';
-  double _priceValue;
+  // String _titleValue = '';
+  // String _descriptionValue = '';
+  // double _priceValue;
+
+  final Map<String, dynamic> _formData = {
+    'title': null,
+    'description': null,
+    'price': null,
+    'image': 'assets/food.jpg',
+  };
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
@@ -28,9 +35,7 @@ class _ProductCreatePage extends State<ProductCreatePage> {
       },
       decoration: InputDecoration(labelText: 'Product Title'),
       onSaved: (String value) {
-        setState(() {
-          _titleValue = value;
-        });
+        _formData['title'] = value;
       },
     );
   }
@@ -48,9 +53,7 @@ class _ProductCreatePage extends State<ProductCreatePage> {
         labelText: 'Product Description',
       ),
       onSaved: (String value) {
-        setState(() {
-          _descriptionValue = value;
-        });
+        _formData['description'] = value;
       },
     );
   }
@@ -63,14 +66,13 @@ class _ProductCreatePage extends State<ProductCreatePage> {
       ),
       validator: (String value) {
         // if (value.trim().length <= 0) {
-        if (value.isEmpty || !RegExp(r'^(?:[1-9]\d*|0)?(?:\.\d+)?$').hasMatch(value)) {
+        if (value.isEmpty ||
+            !RegExp(r'^(?:[1-9]\d*|0)?(?:\.\d+)?$').hasMatch(value)) {
           return 'Price is required and should be a number.';
         }
       },
       onSaved: (String value) {
-        setState(() {
-          _priceValue = double.parse(value);
-        });
+        _formData['price'] = double.parse(value);
       },
     );
   }
@@ -80,15 +82,8 @@ class _ProductCreatePage extends State<ProductCreatePage> {
       return;
     }
     _formKey.currentState.save();
-    
-    final Map<String, dynamic> product = {
-      'title': _titleValue,
-      'description': _descriptionValue,
-      'price': _priceValue,
-      'image': 'assets/food.jpg',
-    };
 
-    widget.addProduct(product);
+    widget.addProduct(_formData);
     Navigator.pushReplacementNamed(context, '/products');
   }
 
@@ -98,30 +93,35 @@ class _ProductCreatePage extends State<ProductCreatePage> {
     final double targetWidth = deviceWidth > 550.0 ? 500.0 : deviceWidth * 0.95;
     final double targetPadding = deviceWidth - targetWidth;
 
-    return Container(
-      margin: EdgeInsets.all(10.0),
-      child: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
-          children: <Widget>[
-            _buildTitleTextDield(),
-            _buildDescriptionTextField(),
-            _buildPriceTextField(),
-            RaisedButton(
-              textColor: Colors.white,
-              child: Text('Save'),
-              onPressed: _submitForm,
-            ),
-            // GestureDetector(
-            //   onTap: _submitForm,
-            //   child: Container(
-            //     color: Colors.green,
-            //     padding: EdgeInsets.all(5.0),
-            //     child: Text('My Button'),
-            //   ),
-            // )
-          ],
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
+            children: <Widget>[
+              _buildTitleTextDield(),
+              _buildDescriptionTextField(),
+              _buildPriceTextField(),
+              RaisedButton(
+                textColor: Colors.white,
+                child: Text('Save'),
+                onPressed: _submitForm,
+              ),
+              // GestureDetector(
+              //   onTap: _submitForm,
+              //   child: Container(
+              //     color: Colors.green,
+              //     padding: EdgeInsets.all(5.0),
+              //     child: Text('My Button'),
+              //   ),
+              // )
+            ],
+          ),
         ),
       ),
     );
