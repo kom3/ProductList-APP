88a857e6743142360fc11a388a79f13c980db9fe
Anoite Team <askaeks@gmail.com>
2019-02-05 15:32:20 +0700

    Creating the favorite button and call notifyListeners() on update

--- lib/scoped-models/products.dart
+++ lib/scoped-models/products.dart
@@ -17,22 +17,40 @@ class ProductsModel extends Model {
     if (_selectedProductIndex == null) {
       return null;
     }
-      
+
     return _products[_selectedProductIndex];
   }
 
   void addProduct(Product product) {
     _products.add(product);
+    _selectedProductIndex = null;
+    notifyListeners();
   }
 
   void updateProduct(Product product) {
     _products[_selectedProductIndex] = product;
     _selectedProductIndex = null;
+    notifyListeners();
   }
 
   void deleteProduct() {
     _products.removeAt(_selectedProductIndex);
     _selectedProductIndex = null;
+    notifyListeners();
+  }
+
+  void toggleProductFavoriteState() {
+    final bool currentFavorite = selectedProoduct.isFavorite;
+    final bool newFavorite = !currentFavorite;
+    final Product updatedProduct = Product(
+        title: selectedProoduct.title,
+        description: selectedProoduct.description,
+        price: selectedProoduct.price,
+        image: selectedProoduct.image,
+        isFavorite: newFavorite);
+    
+    this.updateProduct(updatedProduct);
+    notifyListeners();
   }
 
   void selectProduct(int index) {
