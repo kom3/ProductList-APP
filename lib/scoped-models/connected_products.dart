a99dc8685acc4ecd8cb21b3c6779bbe11740be31
Anoite Team <askaeks@gmail.com>
2019-02-06 18:18:08 +0700

    Using async and await syntax

--- lib/scoped-models/connected_products.dart
+++ lib/scoped-models/connected_products.dart
@@ -15,7 +15,7 @@ mixin ConnectedProductsModel on Model {
   bool _isLoading = false;
 
   Future<bool> addProduct(
-      String title, String description, String image, double price) {
+      String title, String description, String image, double price) async {
     _isLoading = true;
     notifyListeners();
     final Map<String, dynamic> productData = {
@@ -28,10 +28,11 @@ mixin ConnectedProductsModel on Model {
       'userId': _authenticatedUser.id
     };
 
-    return http
-        .post('https://flutter-products-first-app.firebaseio.com/products.json',
-            body: json.encode(productData))
-        .then((http.Response response) {
+    try {
+      final http.Response response = await http.post(
+          'https://flutter-products-first-app.firebaseio.com/products.json',
+          body: json.encode(productData));
+
       final Map<String, dynamic> responseData = json.decode(response.body);
       final Product newProduct = Product(
           id: responseData['name'],
@@ -44,11 +45,11 @@ mixin ConnectedProductsModel on Model {
       _products.add(newProduct);
       notifyListeners();
       return true;
-    }).catchError((error) {
+    } catch (error) {
       _isLoading = false;
       notifyListeners();
       return false;
-    });
+    }
   }
 }
 
